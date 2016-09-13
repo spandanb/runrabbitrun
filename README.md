34dd0db4707600faa70f58bd03b386040a9e79f8
Spandan Bemby <spandan.777@gmail.com>
2016-09-13 21:47:36 +0000

    Hello Mr. Walker
    Changes to be committed:
    	new file:   .gitignore
    	new file:   README.md
    	new file:   data/TRAFFIC_Signals.geojson
    	new file:   data/walked_paths.geojson
            new file:   genwalk.py
    	new file:   socgraph.py

---
+++ README.md
@@ -0,0 +1,16 @@
+
+Data Sets
+---------
+
+The network data is located at: http://snap.stanford.edu/data/index.html#socnets
+
+The walking data is derived from: https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/master/Traffic/Traffic_Signals/TRAFFIC_Signals.geojson
+
+Files
+-----
+genwalk.py - generate walking data
+
+socgraph.py - experimenting with social graph data
+
+data/ - various data files
+
---
+++ genwalk.py
@@ -0,0 +1,47 @@
+import json, pdb
+import random
+
+def genwalk():
+    """
+    Generate walking data. Using randwalk with step=0.0001, n=50
+    generates walks about 0.5 miles.
+    """
+    gis_file = "./data/TRAFFIC_Signals.geojson"
+    path_gis_file = "./data//walked_paths.geojson"
+
+    with open(gis_file) as fptr:
+        fdata = json.load(fptr)
+        outf = open(path_gis_file, "w") #output file
+
+        for point in fdata["features"]:
+            res = randwalk(*point["geometry"]["coordinates"])
+            outf.write(json.dumps(res))
+            outf.write("\n")
+
+        outf.close()
+
+def randwalk(x, y, step=0.0001, n=50):
+    """
+    Generates a random walk starting 
+    at point `x`, `y` (lat, long), with `step` size and takes
+    `n` steps.
+    Returns the vector of positions
+    """
+    
+    xstep = step * random.choice([-1, 1])
+    ystep = step * random.choice([-1, 1])
+
+    posvect = [(x, y)]
+
+    for i in range(n):
+        #skewed in one direction so there is an actual walk
+        r = random.randint(-2, 4) 
+        x += xstep * r
+        r = random.randint(-2, 4) 
+        y += ystep * r
+        posvect.append((x, y))
+
+    return posvect
+
+if __name__ == "__main__":
+    genwalk()
