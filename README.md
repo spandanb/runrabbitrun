2d43ccf43526c0d3ba9b2c7640f3dde7d1721390
Spandan Bemby <spandan.777@gmail.com>
2016-09-13 23:18:04 +0000

    Added python spark word count example

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ The network data is located at: http://snap.stanford.edu/data/index.html#socnets
 
 The walking data is derived from: https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/master/Traffic/Traffic_Signals/TRAFFIC_Signals.geojson
 
+Running a pyspark job:
+    REPL: $SPARK_HOME/bin/pyspark --master spark://<<master-hostname>>:7077
+    Batch: $SPARK_HOME/bin/spark-submit <<python file>>
+
 Files
 -----
 genwalk.py - generate walking data
---
+++ spark-wordcount.py
@@ -0,0 +1,29 @@
+from pyspark import SparkConf, SparkContext
+import os
+
+
+def wordcount():
+    "Performs word count on the file in hdfs: user/test.txt"
+
+    #Create a config object
+    conf = (SparkConf()
+             .setMaster("local")
+             .setAppName("My app")
+             .set("spark.executor.memory", "1g"))
+    
+    #Get spark context
+    sc = SparkContext(conf = conf)
+   
+    public_dns = os.environ["PUBLIC_DNS"]
+    filepath = "hdfs://{}:9000/user/test.txt".format(public_dns)
+    fptr = sc.textfptr(filepath)
+    counts = fptr.flatMap(lambda line: line.split(" "))\
+                 .map(lambda word: (word, 1))\
+                 .reduceByKey(lambda a, b: a + b)
+    
+    res = counts.collect()
+    for val in res:
+        print val
+
+if __name__ == "__main__":
+    wordcount()
