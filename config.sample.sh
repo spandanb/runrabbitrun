5e6505d2d356320f8f6beac6102b933340d9a28e
Spandan Bemby <spandan.777@gmail.com>
2016-09-22 19:27:39 +0000

    Added more env vars to conf

--- config.sample.sh
+++ config.sample.sh
@@ -2,3 +2,6 @@
 
 export PUBLIC_DNS="ec2-52-32-170-79.us-west-2.compute.amazonaws.com"
 export KAFKA_TOPIC="running-data"
+export ZOOKEEPER_LOC="localhost:2181"
+export KAFKA_BROKERS="ec2-52-32-170-79.us-west-2.compute.amazonaws.com:9092, ec2-52-39-193-214.us-west-2.compute.amazonaws.com:9092, ec2-54-70-8-225.us-west-2.compute.amazonaws.com:9092, ec2-52-42-89-132.us-west-2.compute.amazonaws.com:9092"
+
