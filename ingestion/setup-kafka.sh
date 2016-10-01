c264b41b99536a43ea0824eee0a1a08abd9144db
Spandan <spandan.777@gmail.com>
2016-10-01 22:34:13 +0000

    Create Kafka topics script

--- ingestion/setup-kafka.sh
+++ ingestion/setup-kafka.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-#Run kafka
-sudo /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties &
+source ../config.sample.sh
 
 #Creates the topic running-data
-/usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic running-data
+#/usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 2 --partitions 1 --topic $KAFKA_TOPIC
+
+#Creates the topic computed-vals
+/usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 2 --partitions 1 --topic $KAFKA_TOPIC_RES
