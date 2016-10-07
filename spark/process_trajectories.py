98eb36ee665e4f77ff9d3e7ad0fed0a9b8f299ef
Spandan Bemby <spandan.777@gmail.com>
2016-10-07 02:15:54 +0000

    Fixes to spark jobs

--- spark/process_trajectories.py
+++ spark/process_trajectories.py
@@ -138,7 +138,9 @@ def main():
     logger.LogManager.getLogger("akka").setLevel( logger.Level.ERROR )
 
     #iterate over all trajectory files and write to elastic
-    for user in get_users(hdfs, root="/Synthetic_Trajectories/Synthetic_Trajectories"):
+    #for user in get_users(hdfs, root="/Geolife_Trajectories/Data/"):
+    #for user in get_users(hdfs, root="/Synthetic_Trajectories/Synthetic_Trajectories"):
+    for user in get_users(hdfs, root="/Insight_Trajectories/insight/"):
         #iterate over trajectories of this user
         for trajectory in get_trajectories(hdfs, user):
             to_elastic(ew, sc, trajectory['path'])
